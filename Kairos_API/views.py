2289c4120e70d8a096cc6ecac0810fa8c9af1e27
Enzo-py <85191552+Enzo-py@users.noreply.github.com>
2024-01-31 18:57:37 +0100

    feat: Update generator path

--- Kairos_API/views.py
+++ Kairos_API/views.py
@@ -2,19 +2,10 @@ from django.http import JsonResponse
 
 def home(request):
     api_paths = [
-        {"path": "generate_edt/", "POST": "[{'id': int, 'id_prof': int, 'duree': int, 'semaine': int, 'annee': int}]"}
+        {"path": "generate_edt/", "POST": "[{'id_banque': int, 'semaine': int, 'annee': int}]"}
     ]
     # db = Database.get()
     # print(db.run("SELECT * FROM Salle").fetch())
     # db.close()
-    [
-        {"id": 1, "id_prof": 1, "duree": 2},
-        {"id": 2, "id_prof": 1, "duree": 4},
-        {"id": 3, "id_prof": 2, "duree": 2},
-        {"id": 4, "id_prof": 2, "duree": 3},
-        {"id": 5, "id_prof": 3, "duree": 2},
-        {"id": 6, "id_prof": 4, "duree": 2},
-        {"id": 7, "id_prof": 4, "duree": 3}
-    ]
     return JsonResponse(api_paths, safe=False)
 
