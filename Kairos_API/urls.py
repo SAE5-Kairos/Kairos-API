c9cda9319d0137c8bf2834d147ebc660d0525575
gael <gael.grosset@etud.u-picardie.fr>
2024-03-07 20:55:06 +0100

    MAJ - Routes get_all indisponibilités

--- Kairos_API/urls.py
+++ Kairos_API/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
 
     # Génerateur
     path('api/generate_edt/', views_generator.generate_edt),
+    path('api/get_prof_dispo/<int:semaine>/<int:annee>/', views_generator.get_prof_dispo_all),
     path('api/get_prof_dispo/<int:id_prof>/<int:semaine>/<int:annee>/', views_generator.get_prof_dispo),
 
     # Enseigne
