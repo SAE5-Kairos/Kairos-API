eaf7ee923b8185d6c755a1d62d10ec619dcd1257
Enzo-py <85191552+Enzo-py@users.noreply.github.com>
2024-04-03 14:58:15 +0200

    fix: Clear code

--- EDT_generator/V2/generateur.py
+++ EDT_generator/V2/generateur.py
@@ -12,7 +12,6 @@ best_edt_lock = multiprocessing.Lock()
 
 class Manager:
     NB_WORKERS = 30
-    NB_SUPER_WORKERS = 0
     COEF_GAMMA_OVER_EPSILON = 0.95
 
     async def start(self, gamma, epsilon):
@@ -29,18 +28,6 @@ class Manager:
 
         if not all(result) or len(result) != len(tasks):
             print("Un Worker a rencontré une erreur")
-            return False
-
-        try:
-            super_workers = [SuperWorker() for _ in range(self.NB_SUPER_WORKERS)]
-            tasks = [loop.create_task(super_worker.run()) for super_worker in super_workers]
-            result = await asyncio.gather(*tasks)
-        except Exception as e:
-            print("Lors des superworker:", e)
-            return False
-
-        if not all(result) or len(result) != len(tasks):
-            print("Un SuperWorker a rencontré une erreur")
             return False
         
         with open("result.txt", "a") as f:
@@ -240,14 +227,14 @@ class Worker:
                 # 5. Enregister les Phéromones des nouvelles associations
                 # 6. Retirer les nouvelles associations
                 for new_assoc in new_assocs:
-                    #db.run([sql_insert_pheromone, (new_assoc[3], new_score)])
+                    db.run([sql_insert_pheromone, (new_assoc[3], new_score)])
                     tmp_cours: Cours2 = new_assoc[2].copy()
                     tmp_cours.jour = new_assoc[0]
                     tmp_cours.heure = new_assoc[1]
                     self.edt.remove_cours(tmp_cours)
                 
                 # 7. Retirer le cours ajouté
-                #db.run([sql_insert_pheromone, (assoc['ID'], new_score)])
+                db.run([sql_insert_pheromone, (assoc['ID'], new_score)])
                 main_cours = cours.copy()
                 main_cours.jour = assoc['JOUR']
                 main_cours.heure = assoc['HEURE']
@@ -261,96 +248,6 @@ class Worker:
                 self.edt.add_cours(cours, cours.jour, cours.heure)
         db.close()
        
-
-class SuperWorker:
-
-    def __init__(self) -> None:
-        self.initial_score = best_score.value
-        self.edt = EDT2(_from_cours=best_edt)
-        
-        self.omega = {'others': []}
-
-        db = Database.get("edt_generator")
-        for cours in Cours2.ALL:
-            if cours in self.edt.cours:
-                cours = self.edt.cours[self.edt.cours.index(cours)]
-                self.omega[cours] = db.run(f"SELECT * FROM ALL_ASSOCIATIONS WHERE ID_COURS = {cours.id} AND JOUR = {cours.jour}").fetch()
-            else:
-                self.omega['others'].extend(db.run(f"SELECT * FROM ALL_ASSOCIATIONS WHERE ID_COURS = {cours.id} ORDER BY NB_CRENEAUX").fetch())
-        db.close()
-
-        for cours in self.omega:
-            if cours == 'others':
-                continue
-            new_omega = []
-            for assoc in self.omega[cours]:
-                keep = True
-                for other_assoc in self.omega['others']:
-                    if assoc['JOUR'] == other_assoc['JOUR'] and assoc['HEURE'] == other_assoc['HEURE']:
-                        keep = False
-                if keep: new_omega.append(assoc)
-            self.omega[cours] = new_omega
-    
-    async def run(self):
-        for cours in self.edt.cours.copy():
-            if cours not in self.omega:
-                continue
-            
-            free_assoc = []
-            try: self.edt.remove_cours(cours)
-            except: return False
-
-            for assoc in self.omega[cours]:
-                if self.edt.is_free(assoc['JOUR'], assoc['HEURE'], cours) == 1:
-                    free_assoc.append(assoc)
-
-            if not free_assoc:
-                try: self.edt.add_cours(cours, cours.jour, cours.heure)
-                except: return False
-                continue
-
-            assoc = random.choice(free_assoc)
-            try:
-                self.edt.add_cours(cours, assoc['JOUR'], assoc['HEURE'])
-                while True:
-                    node = self.choose_node()
-                    if node is None:
-                        break
-                    self.edt.add_cours(node['COURS'], node['JOUR'], node['HEURE'])
-                    print("Hello from active SuperWorker")
-            except Exception as e:
-                print("SuperWorker:", e)
-                continue
-        
-        score = self.edt.get_score()
-        if self.initial_score == score:
-            return True
-        
-        # Sauvegarder les Phéromones
-        sql_insert_pheromone = """
-            INSERT INTO PHEROMONES2 (ID_ASSOCIATION, PHEROMONE, FROM_SUPER_WORKER) VALUES (%s, %s, 1)
-        """
-        db = Database.get("edt_generator")
-        for cours in self.edt.cours:
-            assoc_id = db.run(f"SELECT ID FROM ALL_ASSOCIATIONS WHERE ID_COURS = {cours.id} AND JOUR = {cours.jour} AND HEURE = {cours.heure}").fetch(first=True)['ID']
-            db.run([sql_insert_pheromone, (assoc_id, score)])
-        
-        db.close()
-
-        if self.edt.get_score() > best_score.value:
-            with best_score_lock:
-                if self.edt.get_score() > best_score.value:
-                    best_score.value = self.edt.get_score()
-                    best_edt[:] = self.edt.cours
-        
-        return True
-    
-    def choose_node(self):
-        for node in self.omega['others']:
-            if self.edt.is_free(node['JOUR'], node['HEURE'], node['COURS']) == 1:
-                return node
-        return None
-
 def generate():
     num_cores = multiprocessing.cpu_count() - 1
 
@@ -380,36 +277,8 @@ def generate():
             # Attendre que toutes les tâches soient terminées
             pool.join()
 
-            print(f"---> {Manager.NB_WORKERS * num_cores} workers et {Manager.NB_SUPER_WORKERS * num_cores} SuperWorkers ont fini de travailler")
+            print(f"---> {Manager.NB_WORKERS * num_cores} workers ont fini de travailler")
             print(f"---> Meilleur score: {best_score}")
 
-    print(f"===> {Manager.NB_WORKERS * num_cores * total_iterations} workers et {Manager.NB_SUPER_WORKERS * num_cores * total_iterations} SuperWorkers ont fini de travailler")
+    print(f"===> {Manager.NB_WORKERS * num_cores * total_iterations} workers ont fini de travailler")
     return EDT2(_from_cours=best_edt)
-
-# def process_loop(start, end):
-#     for i in range(start, end): ...
-#         # Faites quelque chose avec la variable i
-
-# if __name__ == '__main__':
-#     num_cores = multiprocessing.cpu_count()
-#     pool = multiprocessing.Pool(processes=num_cores)
-
-#     total_iterations = 1_000_000_000_000_000
-#     chunk_size = total_iterations // num_cores
-
-#     start = 0
-#     end = chunk_size
-
-#     results = []
-
-#     for _ in range(num_cores):
-#         results.append(pool.apply_async(process_loop, (start, end)))
-#         start = end
-#         end += chunk_size
-
-#     pool.close()
-#     pool.join()
-
-#     # Récupérez les résultats si nécessaire
-#     for result in results:
-#         result.get()
